38f4ddebcb49a42d9eb06bb37fb4e9e13b5f3d8b
Badarinath Venkatnarayansetty <badarinath.venkatnarayansetty@macys.com>
2017-10-14 17:58:01 -0700

    added price validations

---
+++ UnitTestsTests/TestStructure.swift
@@ -0,0 +1,33 @@
+//
+//  TestStructure.swift
+//  UnitTests
+//
+//  Created by Badarinath Venkatnarayansetty on 10/14/17.
+//  Copyright © 2017 Badarinath Venkatnarayansetty. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+
+class TestStructure: QuickSpec{
+    
+    override func spec() {
+        
+        describe("Component to be Verified"){
+            
+            beforeEach {
+                //set up data
+            }
+            
+            context("State of Test Case"){
+                
+                it(" Expect Validations "){
+                    
+                }
+                
+            }
+        }
+    }
+}
--- UnitTestsTests/WatchTest.swift
+++ UnitTestsTests/WatchTest.swift
@@ -15,29 +15,30 @@ import Quick
 class Dummy{}
 
 class WatchTest: QuickSpec{
-    
-    var watchInformation: [Watch] = []
-    var watch : Watch!
-    
+
     override func spec() {
+        
+        var watchInformation: [Watch]!
+        var watch : Watch!
         
         describe("Modal Checks"){
             
             beforeEach {
-                self.watchInformation = WatchDataHelper.getWatchInformation()
-                self.watch = self.watchInformation[0]
+                watchInformation = WatchDataHelper.getWatchInformation()
+                watch = watchInformation[0]
+                //watch = Watch(name: "Men's Business Quartz Watch CAGARNY 6828", price: 11.69, brand: Brand(id: 6828, name: "CAGARNY"))
             }
             
             context("When Watch Model Data is available"){
                 it("Model Type Class Validations on Watch Object "){
                     
                     //class check
-                    expect(self.watch).to(beAKindOf(Watch.self))
-                    expect(self.watch).toNot(beAKindOf(Dummy.self))
+                    expect(watch).to(beAKindOf(Watch.self))
+                    expect(watch).toNot(beAKindOf(Dummy.self))
                     
                     //instance check
-                    expect(self.watch).to(beAnInstanceOf(Watch.self))
-                    expect(self.watch).toNot(beAnInstanceOf(Dummy.self))
+                    expect(watch).to(beAnInstanceOf(Watch.self))
+                    expect(watch).toNot(beAnInstanceOf(Dummy.self))
                 }
             }
             
@@ -45,17 +46,37 @@ class WatchTest: QuickSpec{
                 var brandInformation: Brand!
                     
                 beforeEach{
-                    brandInformation = self.watch.brand
+                    brandInformation = watch.brand
                 }
                 
                 it("Watch Name and Brand validations") {
                     
                     //string and name presence check
-                    expect(self.watch.name).to(beAnInstanceOf(String.self))
-                    expect(self.watch.name).to(contain("Men's Business Quartz Watch CAGNARY 6828"))
+                    expect(watch.name).to(beAnInstanceOf(String.self))
+                    expect(watch.name).to(contain("Men's Business Quartz Watch CAGARNY"))
+                    expect(watch.name).to(endWith("CAGARNY 6828"))
                     
                     expect(brandInformation.name).to(equal("CAGARNY"))
+                    expect(brandInformation.name).to(beginWith("CAG"))
                     expect(brandInformation.name).toNot(beNil())
+                }
+                
+                it("Watch Price validations"){
+                  
+                    expect(watch.price).toNot(beNil())
+                    expect(watch.price).to(beTruthy())
+                    
+                    //evaluating boundary cases
+                    expect(watch.price).toNot(beLessThan(0))
+                    expect(watch.price).to(beCloseTo(11, within: 0.69))
+                    expect(watch.price).to(beLessThanOrEqualTo(11.69))
+                    expect(watch.price).to(beGreaterThan(Double.pi))
+                    
+                    expect(watch.price).toNot(beGreaterThan(11.69))
+                    expect(watch.price).toNot(beGreaterThan(Double.infinity))
+                    expect(watch.price).toNot(be(Double.signalingNaN))
+                    expect(watch.price).toNot(be(Double.nan))
+                
                 }
             }
         }
